348298396635bae88395d3ac29d2d358b23e6e43
1335926665@qq.com <qaz753>
2020-04-23 13:46:57 +0800

    https://github.com/louis-zhang-lei/hexis-master.git

---
+++ src/views/home/Home.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="hello">
+    <div class="jianbianImg">
+      <jianbianImg></jianbianImg>
+    </div>
+    <div class="zhuti">
+      <zhengwen></zhengwen>
+    </div>
+  </div>
+</template>
+
+<script>
+  import jianbianImg from "@/views/home/layout/jianbianImg";
+  import zhengwen from "@/views/home/layout/zhengwen";
+  export default {
+    name: 'HelloWorld',
+    components:{
+      top,
+      jianbianImg,
+      zhengwen
+    },
+    data () {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .hello{
+    height: 100%;
+  }
+  .top{
+    width: 100%;
+    position: absolute;
+    top: 0;
+    z-index: 1000;
+  }
+  .jianbianImg{
+    width: 100%;
+    /* margin-top: 10rem; */
+
+  }
+  .zhuti{
+    width: 100%;
+    /*height: 100rem;*/
+  }
+
+</style>
